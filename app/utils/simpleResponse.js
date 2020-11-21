b5ddb881a7ed69e111b028ebc88714aac953d0a2
BackRunner <dev@backrunner.top>
2020-11-22 02:46:03 +0800

    add: cache header

--- app/utils/simpleResponse.js
+++ app/utils/simpleResponse.js
@@ -3,4 +3,8 @@
 module.exports = (ctx, data) => {
   ctx.status = 200;
   ctx.body = data;
+  ctx.set({
+    'Access-Control-Expose-Headers': 'pixiviz-cache',
+    'pixiviz-cache': 'true',
+  });
 };
