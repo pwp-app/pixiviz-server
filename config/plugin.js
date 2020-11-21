24f7d77bc63279b0cd0535d6a2cdc6851fcba4d6
BackRunner <dev@backrunner.top>
2020-11-22 02:44:35 +0800

    minor: format

--- config/plugin.js
+++ config/plugin.js
@@ -2,16 +2,16 @@
 
 /** @type Egg.EggPlugin */
 module.exports = {
-    cors: {
-        enable: true,
-        package: 'egg-cors',
-    },
-    validate: {
-        enable: true,
-        package: 'egg-validate',
-    },
-    redis: {
-        enable: true,
-        package: 'egg-redis',
-    },
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
+  validate: {
+    enable: true,
+    package: 'egg-validate',
+  },
+  redis: {
+    enable: true,
+    package: 'egg-redis',
+  },
 };
