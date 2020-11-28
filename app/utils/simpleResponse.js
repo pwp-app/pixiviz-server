55ac191396263abb30c426eee0b2ad703c2b8f5e
BackRunner <dev@backrunner.top>
2020-11-29 02:23:07 +0800

    remove: expose header

--- app/utils/simpleResponse.js
+++ app/utils/simpleResponse.js
@@ -3,8 +3,4 @@
 module.exports = (ctx, data) => {
   ctx.status = 200;
   ctx.body = data;
-  ctx.set({
-    'Access-Control-Expose-Headers': 'pixiviz-cache',
-    'pixiviz-cache': 'true',
-  });
 };
